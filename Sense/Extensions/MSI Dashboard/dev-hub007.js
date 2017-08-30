f862e8d49bf0b62fb54a16e4da58857b50b88ee1
dsw418 <dsw418@gmail.com>
2017-08-30 16:39:54 -0400

    Updated the Layout

--- Sense/Extensions/MSI Dashboard/dev-hub007.js
+++ Sense/Extensions/MSI Dashboard/dev-hub007.js
@@ -30,8 +30,43 @@ require(["js/qlik"], function (qlik) {
 
 
 	//get objects -- inserted here --
-	app.getObject('QV2-01','yJmwu');
+	app.getObject('KPI-07','yVjvP');
+	app.getObject('KPI-06','gLQSS');
+	app.getObject('KPI-05','dfJyHD');
+	app.getObject('KPI-04','AJjKyx');
+	app.getObject('KPI-03','dfJyHD');
+	
+	app.getObject('QV2-04s','NwMgmw');
+	app.getObject('QV3-05-2','PQaQx');
+	app.getObject('QV3-05','rFfgVjy');
+	app.getObject('QV3-04-2','akMrJy');
+	app.getObject('QV3-04','QyTjZq');
+	app.getObject('QV3-03-2','HZnKBF');
+	app.getObject('QV3-03','pjJFm');
+	app.getObject('QV3-02-2','pPTeg');
+	app.getObject('QV3-02','fuejJ');
+	app.getObject('QV3-01','yJmwu');
+	app.getObject('KPI-09','yVjvP');
+	app.getObject('KPI-08','gLQSS');
+	
+	
+	
+	
+	app.getObject('QV2-03','07d9a6b7-2644-4bd0-aa21-38ed28dc6888');
+	app.getObject('QV2-06s','jfzxJcY');
+	
+	app.getObject('QV2-01','xKYDKQ');
+	app.getObject('KPI-02','AJjKyx');
+	app.getObject('KPI-01','dfJyHD');
+	app.getObject('QV1-04s','pgJgbsr');
 	app.getObject('QV1-01','xKYDKQ');
+	
+	
+	
+	
+	
+	
+	
 	
 	app.getObject('QV2-05-2','PQaQx');
 	app.getObject('QV2-04-2','akMrJy');
@@ -39,20 +74,12 @@ require(["js/qlik"], function (qlik) {
 	app.getObject('QV2-02-2','pPTeg');
 	app.getObject('QV2-05','rFfgVjy');
 	app.getObject('QV2-04','QyTjZq');
-	app.getObject('QV2-03','pjJFm');
+	
 	
 	
 	app.getObject('QV2-02','fuejJ');
 	
 	
-	app.getObject('KPI-05','gLQSS');
-	app.getObject('KPI-06','yVjvP');
-	
-	
-	
-	
-	
-	app.getObject('KPI-04','dfJyHD');
 	
 	
 	
@@ -64,12 +91,20 @@ require(["js/qlik"], function (qlik) {
 	
 	
 	
-	app.getObject('KPI-02','AJjKyx');
-	
-	app.getObject('KPI-01','dfJyHD');
 	
 	
-	app.getObject('QV1-04s','pgJgbsr');
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 	
@@ -85,7 +120,7 @@ require(["js/qlik"], function (qlik) {
 	app.getObject('QV1-03','07d9a6b7-2644-4bd0-aa21-38ed28dc6888');
 	
 	
-	app.getObject('KPI-03','wugtRr');
+	
 	
 	
 	
@@ -158,8 +193,8 @@ require(["js/qlik"], function (qlik) {
 	
 	
 	
-	app.getObject('QV3-05','xKYDKQ');
-	app.getObject('QV3-03','fREG');
+	
+	
 	
 	app.getObject('QV1-05s','fREG');
 	
